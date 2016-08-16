6feca013c6399b54e6261374771b91e9d3243baa
kgough <kgough@redhat.com>
2016-08-17 09:41:10 +1000

    linked to zanata bootstrap.css instead of default to fix heading fonts etc

--- style-guide/index.php
+++ style-guide/index.php
@@ -8,7 +8,7 @@
 <meta name="viewport" content="width=device-width">
 <!-- Style Guide Boilerplate Styles -->
 <link rel="stylesheet" href="css/styleguide.css">
-<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="../dist/css/bootstrap.css">
 
 <link rel="stylesheet" href="../sg-style.css">
 </head>
