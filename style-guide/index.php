616afa2c34a73837b309eae229e2d81f86a671ae
kgough <kgough@redhat.com>
2016-08-25 13:51:55 +1000

    fixed file locations

--- style-guide/index.php
+++ style-guide/index.php
@@ -8,7 +8,7 @@
 <meta name="viewport" content="width=device-width">
 <!-- Style Guide Boilerplate Styles -->
 <link rel="stylesheet" href="css/styleguide.css">
-<link rel="stylesheet" href="../dist/css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap.css">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 <link rel="stylesheet" href="../sg-style.css">
 </head>
